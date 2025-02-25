edebcd9bc6ca51df15e4eae8edfe6f2b1cc43e45
fgh_ssh <fghdotio@gmail.com>
2025-02-24 23:41:17 -0600

    doge commitment

--- src/services/doge/restapi.test.ts
+++ src/services/doge/restapi.test.ts
@@ -3,6 +3,8 @@ import { DogeMemoryWalletProvider, createP2PKHTransaction } from 'doge-sdk';
 
 import { DogeRestApiClient } from './restapi';
 
+import * as bitcoin from 'bitcoinjs-lib';
+
 describe('DogeRestApiClient', () => {
   let testnetClient: DogeRestApiClient;
   // dev Crypto APIs
@@ -17,7 +19,7 @@ describe('DogeRestApiClient', () => {
   const testnetAddress: string = 'nsSfZttkEy2ZkujJUmQvKdjsTVRXSAwSvm';
   const testnetMinerAddress: string = 'nmZ36RoFkyd9tKqfTk2iBt5UfgLfbcxC98';
   // nWujuS2wgYTbyL7B3dk7RoLb7KqfEY7tVS
-  const testnetWif: string = 'QWzHvo6EBqEpYADitbmV7Ybq9ufGN3q16fvVc2dXH9egQSpCr64A';
+  const testnetWif: string = '';
 
   describe('postTx', () => {
     it('should return the tx', async () => {
@@ -35,18 +37,43 @@ describe('DogeRestApiClient', () => {
       };
       console.log(utxos.length, formattedUtxo);
 
-      const fee = 2_333_000;
+      const commitment = '10c4a63bbffad8a5a374eb2d56f5790f4bb4b93796f04d9d8de9ee9177038104';
+      // const commitment = 'b4b51b307ab82b41c1eb422284475c8abd28a69702c11e059693cb0b6e8c019b';
+      // https://sochain.com/tx/DOGETEST/9eaaec1a4478f0901df98c33d36173a45618f9d077bc9301e8087a723707a5fb
+      // https://blockexplorer.one/dogecoin/testnet/tx/9eaaec1a4478f0901df98c33d36173a45618f9d077bc9301e8087a723707a5fb
+
+      // const opCode = 'OP_RETURN';
+      // const commitmentOutputString = `${opCode} ${commitment}`;
+      // console.log(getOpCodeFromString(opCode));
+
+      const opReturnScript = bitcoin.script.compile([bitcoin.opcodes.OP_RETURN, Buffer.from(commitment, 'hex')]);
+      const opReturnScriptUint8 = new Uint8Array(opReturnScript);
+      console.log(opReturnScriptUint8, opReturnScript.toString('hex'));
+      const commitmentOutput = {
+        value: 0,
+        script: opReturnScriptUint8,
+      };
+
+      const changeOutput = {
+        address: wallet.address,
+        value: formattedUtxo.value - 10 ** 8,
+      };
+
+      // const fee = 2_333_000;
       const txBuilder = createP2PKHTransaction(wallet, {
         inputs: [formattedUtxo],
-        outputs: [
-          { address: testnetAddress, value: 280_000 },
-          { address: wallet.address, value: formattedUtxo.value - fee },
-        ],
+        outputs: [commitmentOutput, changeOutput],
         address: wallet.address,
       });
       const finalizedTx = await txBuilder.finalizeAndSign();
       const txHex = finalizedTx.toHex();
-      console.log(await testnetClient.postTx({ txHex }));
+      console.log(txHex);
+      console.log(
+        await testnetClient.postTx({
+          txHex:
+            '0200000001fba50737727a08e80193bc77d0f91856a47361d3338cf91d90f078441aecaa9e010000006a473044022015eb9e548bf5fb42d72cf59d5eded7ac50b804b45f1a34348591d0f9074e946902201a16512e45e8597ed1a72d59a0166e02ea3105627cd55bf60cba1a97858d0b7601210201ed4ceaf7982e3a8de9aac1316244302fdea30831b9dd8f8563fe87e02a5f6effffffff030000000000000000226a2010c4a63bbffad8a5a374eb2d56f5790f4bb4b93796f04d9d8de9ee917703810422020000000000001976a914b74bd483e3da461a03d1717d0a600b09a49dd53588ac00fea237c12fbd0d1976a914b74bd483e3da461a03d1717d0a600b09a49dd53588ac00000000',
+        }),
+      );
     });
   });
 
@@ -111,6 +138,6 @@ describe('DogeRestApiClient', () => {
 });
 
 /* 
+pnpm vitest run src/services/doge/restapi.test.ts -t "postTx"
 pnpm vitest run src/services/doge/restapi.test.ts -t "getTx should return the transaction"
-pnpm vitest run src/services/doge/restapi.test.ts -t "getAddressTxs should return the transactions"
 */
